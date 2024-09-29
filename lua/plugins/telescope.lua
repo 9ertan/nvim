e6aaff7b1b5cb2d971ddb6aa8f08f50b6b92f1b5
bertan <git@brtn.fyi>
2024-09-29 05:49:34 +0100

    feat(plugins/telescope): configure telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,13 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    require('telescope').setup {}
+    require('telescope').setup {
+      defaults = {
+        file_ignore_patterns = {
+          'node_modules',
+          'venv',
+        },
+      },
+    }
   end,
 }
