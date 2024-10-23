1b21e6fbd2926e4829f73bb278e8ca767f0a306f
b3rtan <git@brtn.fyi>
2024-10-23 05:51:23 +0100

    feat(plugins/cmp): add proper completion navigation

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -32,6 +32,11 @@ return {
             fallback()
           end
         end, { 'i', 's' }),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-l>'] = cmp.mapping.confirm { select = true, behavior = cmp.ConfirmBehavior.Insert },
       },
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
